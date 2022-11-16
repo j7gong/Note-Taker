519fea17004a86d2ef5c6d6daf41d893934f3d5b
Jingsi <gongjingsi@hotmail.com>
2022-11-16 10:40:01 -0500

    add routes for finding notes by ID

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 };
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     
@@ -25,8 +30,16 @@ app.get('/api/notes', (req, res) => {
     }
 
     res.json(results);
-    console.log(res);
   });
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
