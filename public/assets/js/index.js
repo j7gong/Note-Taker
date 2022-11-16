8b5d1995de0791836b5d69a18a2d870bb54544c4
Jingsi <gongjingsi@hotmail.com>
2022-11-16 11:37:27 -0500

    adjust linkage due to folder structure change

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -37,10 +37,21 @@ const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
     headers: {
+      // Accept: 'application/json',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
-  });
+  })
+  // .then(response => {
+  //   if(response.ok) {
+  //     return response.json();
+  //   }
+  //   alert('Error: ' + response.statusText);
+  // }).then(postResponse => {
+  //   console.log(postResponse);
+  //   alert('Thank you for adding an animal!');
+  // })
+  ;
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming tring or array data
 app.use(express.urlencoded({extended: true}));
 
@@ -83,6 +85,10 @@ app.post('/api/notes', (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note);
     };
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
